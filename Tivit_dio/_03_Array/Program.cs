fc5792ab84066b84e9e4d25b6c18d1183fc1db02
Junio Roque <jcjunioroque@gmail.com>
2026-03-08 18:48:27 -0300

    first commit

---
+++ Tivit_dio/_03_Array/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] arrayInteiro = new int[4];
+
+arrayInteiro[0] = 72;
+arrayInteiro[1] = 64;
+arrayInteiro[2] = 50;
+arrayInteiro[3] = 1;
+
+for(int contador = 0; contador < arrayInteiro.Length; contador++) {
+    Console.WriteLine($"Posição N° {contador} - {arrayInteiro[contador]}");
+}
---
+++ Tivit_dio/_04_Foreach/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] arrayInteiro = new int[3];
+arrayInteiro[0] = 3;
+arrayInteiro[1] = 4;
+arrayInteiro[2] = 11;
+
+arrayInteiro[0] = 333;
+
+foreach(int valor in arrayInteiro){
+    Console.WriteLine("Posição N° "+valor);
+}
