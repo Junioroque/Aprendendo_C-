f4e8c434f908490ce789ea4347ea5a8372a0b32d
Junio Roque <jcjunioroque@gmail.com>
2026-04-14 22:17:28 -0300

    Continuando o exercicio do estacionamento

--- Tivit_dio/_06_Projeto_Estacionamento/Estacionamento.cs
+++ Tivit_dio/_06_Projeto_Estacionamento/Estacionamento.cs
@@ -16,17 +16,50 @@ namespace _06_Projeto_Estacionamento
 
         public void AdicionarVeiculo()
         {
-            Console.Write("OI");
+            Console.WriteLine("Digite a placa do ve[iculo para estacionar: ");
         }
 
         public void RemoverVeiculo()
         {
-            Console.Write("OI oi");
+            Console.WriteLine("Digit a placa do veículo para remover: ");
+
+            string placa = " ";
+
+            //verifica se o veiculo existe
+            if(veiculos.Any(x => x.ToUpper() == placa.ToUpper())){
+                console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado: ");
+
+                //TODO: Pedir para o usuário digitar a quantidade de horas que  o veiculo ficou estacionado
+                //TODO: Realizar o seguinte cálculo: "precoInicial + precoFinal * para variavel
+                //IMPRIME AQUI
+                int hora = 0;
+                decimal valorTotal = 0;
+
+                //TODO: Remover a placa digitada da lista de veiculos
+                //IMPREMENTE AQUI
+                Console.WriteLine($"O veiculo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+
+            }else
+            {
+                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui.");
+            }
+
+
         }
 
         public void ListarVeiculos()
         {
-             Console.Write("OI  oi oi");
+            //Verifica se há veículos no estacionamento
+            if (veiculos.Any())
+            {
+                Console.WriteLine("Os veículos estacionados são: ");
+                //TODO: Realizar um laço de repetição, exibindo os veículos estacionados
+                //IMPRIME AQUI
+            }else
+            {
+                Console.Write("Não há veículos estacionados.");
+            }
+             
         }
         
     }
