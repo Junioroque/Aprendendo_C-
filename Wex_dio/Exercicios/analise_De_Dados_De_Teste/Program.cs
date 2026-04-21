c4c6ef142a380e9f29a3a71b543efe02cea74cbf
Junio Roque <jcjunioroque@gmail.com>
2026-04-21 20:19:19 -0300

    Mais exercicio wex

---
+++ Wex_dio/Exercicios/analise_De_Dados_De_Teste/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AnaliseDadosTeste {
+  class Program {
+    static void Main(string[] args) {
+      // Solicita ao usuário a quantidade de testes bem-sucedidos:
+      int testesBemSucedidos = int.Parse(Console.ReadLine());
+
+      // Solicita ao usuário a quantidade de testes totais:
+      int testesTotais = int.Parse(Console.ReadLine());
+
+      // TODO: Implemente as condições para o cálculo da taxa de sucesso:
+      //fórmula: taxaSucesso = (double)testesBemSucedidos / testesTotais.
+      double taxaSucesso = ((double)testesBemSucedidos / (double)testesTotais) * 100;
+
+      // TODO: Implemente uma estrutura condicional (if/else) para avaliar a taxa de sucesso e tomar decisões com base nela:
+      // Se a taxa de sucesso for maior ou igual a 80%, o programa exibirá a mensagem "A funcionalidade esta pronta para lancamento."
+      // Caso contrário, se a taxa de sucesso for menor que 80%, o programa exibirá a mensagem "A funcionalidade nao esta pronta para lancamento."
+      if(taxaSucesso >= 80)
+      {
+         Console.WriteLine("A funcionalidade esta pronta para lancamento.");
+      }
+      else
+      {
+         Console.WriteLine("A funcionalidade nao esta pronta para lancamento.");
+      }
+    }
+  }
+}
